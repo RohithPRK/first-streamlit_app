c397012bce830850eda491214d6e76bbb37a5c94
RohithPRK <116796927+RohithPRK@users.noreply.github.com>
2022-10-27 14:27:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('Data Aces')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry metal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('hard boiled free-range egg')
