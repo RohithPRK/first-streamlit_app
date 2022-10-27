8bb8a922407533e4ae6ca2b82e0a2f0ad91c6ed9
RohithPRK <116796927+RohithPRK@users.noreply.github.com>
2022-10-27 14:33:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text('🥣 Omega 3 & Blueberry metal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+stremlit.text('Mixed fruits salad')
